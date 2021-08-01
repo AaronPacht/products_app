cfe78b81c777637af5c9db4e781ac33e45576ae4
AaronPacht <aharonpacht@gmail.com>
2021-08-01 14:40:25 -0400

    added review create functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,25 @@ class ReviewsController < ApplicationController
     def show
         @review=Review.find(params[:id])
     end
+
+    def new
+        @review=Review.new
+        @product=Product.find(params[:product_id])
+    end
+
+    def create
+        @product=Product.find(params[:product_id])
+        @review=Review.new(review_params)
+        @review.product=@product
+        if @review.save
+            redirect_to product_review_url(@review,@review)
+        else
+            render :new
+        end
+    end
+
+    private
+    def review_params
+        params.require(:review).permit(:user,:stars,:message)
+    end
 end
