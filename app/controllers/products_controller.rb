6964b2c68a8764d8b5ef5ea77eed88a62eff7f73
AaronPacht <aharonpacht@gmail.com>
2021-08-01 12:28:22 -0400

    added show for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,9 @@ class ProductsController < ApplicationController
     def index
         @products=Product.all
     end
+
+    def show
+        @product=Product.find(params[:id])
+        @reviews=Review.where(product_id: params[:id])
+    end
 end
