2c01b32cec969ece7476a7f248d47c00c996c7ac
AaronPacht <aharonpacht@gmail.com>
2021-08-01 13:29:33 -0400

    added edit functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,20 @@ class ProductsController < ApplicationController
             render :new
         end
     end
+
+    def edit
+        @product=Product.find(params[:id])
+    end
+
+    def update
+        @product=Product.find(params[:id])
+        if @product.update(product_params)
+            redirect_to product_url(@product)
+        else
+            render :edit
+        end
+    end
+
     private
     def product_params
         params.require(:product).permit(:name,:price,:description,:url)
